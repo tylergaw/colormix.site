be7173aab9d9fa47f145a7401c1296e2d9063c23
Tyler Gaw <me@tylergaw.com>
2022-06-10 01:18:02 -0400

    Improves snippet 0 handling

--- src/components/ColorOutput.tsx
+++ src/components/ColorOutput.tsx
@@ -5,24 +5,35 @@ import store, { initialState as IState, IColor } from "../store.ts";
 
 const initialState = store.get();
 
-const buildColor = (color: IColor): string => {
+const buildColor = (color: IColor, forDisplay?: boolean): string => {
   const { value, percentage } = color;
-  const showPercentage = percentage !== null && percentage > 0;
+  const showPercentage = forDisplay && percentage < 1 ? false : true;
   return `${value}${showPercentage ? ` ${percentage}%` : ""}`;
 };
 
-const buildSnippet = (state: IState): string => {
+interface ISnippet {
+  display: string;
+  use: string;
+}
+
+const buildSnippet = (state: IState): ISnippet => {
   const color1 = state["1"];
   const color2 = state["2"];
-  return `color-mix(in ${state.colorSpace}, ${buildColor(color1)}, ${buildColor(
-    color2
-  )})`;
+  const display = `color-mix(in ${state.colorSpace}, ${buildColor(
+    color1,
+    true
+  )}, ${buildColor(color2, true)})`;
+  const use = `color-mix(in ${state.colorSpace}, ${buildColor(
+    color1
+  )}, ${buildColor(color2)})`;
+
+  return { display, use };
 };
 
 const ColorOutput: FunctionComponent = () => {
   const [isListening, setIsListening] = useState(false);
   const [state, setState] = useState(initialState);
-  const colorMixSnippet = buildSnippet(state);
+  const { display: snippetDisplay, use: snippetUse } = buildSnippet(state);
 
   useEffect(() => {
     if (!isListening) {
@@ -35,13 +46,13 @@ const ColorOutput: FunctionComponent = () => {
   });
 
   return (
-    <div style={{ backgroundColor: colorMixSnippet, flex: 1 }}>
+    <div style={{ backgroundColor: snippetUse, flex: 1 }}>
       <ColorSpaceInput />
 
       <input
         style={{ display: "block", width: "100%" }}
         type="text"
-        value={colorMixSnippet}
+        value={snippetDisplay}
         readonly
       />
     </div>
