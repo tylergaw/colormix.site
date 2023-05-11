a09ac2fd8d5a7e34fa2f7fa3d4758abe244aebb9
Tyler Gaw <me@tylergaw.com>
2023-05-11 19:47:12 -0400

    Adds Icons (#6)
    * Adds astro-icon dep

--- src/components/ColorSpaceInput/index.tsx
+++ src/components/ColorSpaceInput/index.tsx
@@ -32,7 +32,15 @@ const ColorSpaceInput: FunctionComponent = () => {
             <option value={space}>{space}</option>
           ))}
         </select>
-        <img src="/images/arrow-down.svg" alt="" height="24" width="24" />
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M16.59 8.59003L12 13.17L7.41 8.59003L6 10L12 16L18 10L16.59 8.59003Z" />
+        </svg>
       </div>
     </div>
   );
